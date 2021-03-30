9e91170e70db8b13a2804b9f6cfaffd35969309b
radja01 <81616299+radja01@users.noreply.github.com>
2021-03-30 11:08:20 +0200

    Add files via upload

---
+++ imc radja.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Éditeur de Spyder
+
+Ceci est un script temporaire.
+"""
+
+def demande_valeur(message):
+    """str->int"""
+    assert type(message)==str,"Le paramètre n'est pas une chaine"
+    print(message)
+
+
+taille_cm = demande_valeur(2)
+    saisie=input()
+    assert saisie.insdigit(transtyper_en_entier)
+    saisie_entier = 
+     
